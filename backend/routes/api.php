5828472f1eb106919414bb60b9ba461c5612d784
Rogerio Pereira <hi@rogeriopereira.dev>
2024-06-14 19:47:22 -0400

    feat(backend): Add route to register to vote and route to get all voting districts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\VoterController;
 use App\Http\Controllers\Api\VotingDistrictController;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/register-to-vote', [VoterController::class, 'store']);
+Route::get('/voting-districts/open', [VotingDistrictController::class, 'index']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
