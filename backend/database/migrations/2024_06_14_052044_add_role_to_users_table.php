e805628edd7e8c666d74d3a1aed44d218909403e
Rogerio Pereira <hi@rogeriopereira.dev>
2024-06-14 19:50:23 -0400

    refactor: Add "after" statement when adding column role to Users table

--- backend/database/migrations/2024_06_14_052044_add_role_to_users_table.php
+++ backend/database/migrations/2024_06_14_052044_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('Voter');
+            $table->string('role')->default('Voter')->after('remember_token');
         });
     }
 
