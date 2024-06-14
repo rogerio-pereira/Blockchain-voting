43be484f98d1b7284d0e8a1791dbd460284c144d
Rogerio Pereira <hi@rogeriopereira.dev>
2024-06-14 14:57:33 -0400

    feat(backend): Api Routes to Voting Districts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\VotingDistrictController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Logged Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //Admin Routes
+    Route::group(['middleware' => [IsAdmin::class]], function() {
+        Route::resource('/voting-districts', VotingDistrictController::class);
+    });
 });
