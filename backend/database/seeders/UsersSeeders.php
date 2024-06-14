b28221b4406049cd2bb52238ccfe7b25d21443fd
Rogerio Pereira <hi@rogeriopereira.dev>
2024-06-14 01:41:35 -0400

    feat(backend): Users Seeder

---
+++ backend/database/seeders/UsersSeeders.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+        ]);
+        User::factory()->create([
+            'name' => 'Voter',
+            'email' => 'voter@voter.com',
+        ]);
+    }
+}
